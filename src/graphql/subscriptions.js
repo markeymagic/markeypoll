0bc299d8912e47cd02d3037935028d5302da46ac
Chris Markey <chris.markey@versent.com.au>
2019-08-23 01:39:31 +1000

    init

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCastVote = `subscription OnCastVote {
+  onCastVote {
+    id
+    name
+    description
+    votes
+  }
+}
+`;
+export const onCreateCandidate = `subscription OnCreateCandidate {
+  onCreateCandidate {
+    id
+    name
+    description
+    votes
+  }
+}
+`;
+export const onUpdateCandidate = `subscription OnUpdateCandidate {
+  onUpdateCandidate {
+    id
+    name
+    description
+    votes
+  }
+}
+`;
+export const onDeleteCandidate = `subscription OnDeleteCandidate {
+  onDeleteCandidate {
+    id
+    name
+    description
+    votes
+  }
+}
+`;
